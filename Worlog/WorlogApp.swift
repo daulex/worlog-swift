ca6c963e0e560dbbc8d1d7e09f14c4c40ecd6de6
Kirill G <kirillgalenko@gmail.com>
2023-01-09 09:55:34 +0200

    Initial Commit

---
+++ Worlog/WorlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorlogApp.swift
+//  Worlog
+//
+//  Created by Kirills Galenko on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
